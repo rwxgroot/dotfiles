5b2d7bcfca607560293c439c724db84158872935
Sachin <sachmalik1999@gmail.com>
2024-01-31 18:14:44 +0530

    Some more chores

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,7 +7,7 @@ return {
 		"MunifTanjim/nui.nvim"
 	},
 	config = function()
-		vim.keymap.set("n", "<C-e>", ":Neotree focus<CR>", {})
+		vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
 		vim.keymap.set("n", "<C-f>", ":Neotree buffers reveal float<CR>", {})
 	end
 
