61dab0abfdebd8d1047c1339e63bb655f75ffe44
Sachin <sachmalik1999@gmail.com>
2024-03-05 11:33:09 +0530

    Inlay hints and python support

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,110 +1,121 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {"rust_analyzer", "pylsp"}
-            })
-        end
-    },
-    {
-        "j-hui/fidget.nvim",
-        tag = "legacy",
-        opts = {}
-    },
-    {
-        "hrsh7th/cmp-nvim-lsp"
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-        "saadparwaiz1/cmp_luasnip",
-        "rafamadriz/friendly-snippets",
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local cmp = require("cmp")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {"rust_analyzer", "pylsp"}
+			})
+		end
+	},
+	{
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		opts = {}
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp"
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
 
-            require("luasnip.loaders.from_vscode").lazy_load()
-        
-            cmp.setup({
-                snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-                },
-                window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" }, 
-                }, {
-                { name = "buffer" },
-                }),
-            })
-        end,
-    },    
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require('lspconfig')
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, 
+				}, {
+						{ name = "buffer" },
+					}),
+			})
+		end,
+	},    
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require('lspconfig')
+
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 			lspconfig.rust_analyzer.setup{
 				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					vim.lsp.inlay_hint.enable(bufnr)
+				end
 			}
-			lspconfig.pylsp.setup{}
+			lspconfig.pylsp.setup{settings = {
+				pylsp = {
+					plugins = {
+						pycodestyle = {
+							maxLineLength = 100
+						}
+					}
+				}
+			}}
 
-            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+			vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+			vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+			vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+			vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
-            vim.api.nvim_create_autocmd('LspAttach', {
+			vim.api.nvim_create_autocmd('LspAttach', {
 
-            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 
-            callback = function(ev)
-                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+				callback = function(ev)
+					vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-                local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<space>f', function()
-                vim.lsp.buf.format { async = true }
-                end, opts)
-            end,
-            })
-        end
-    }
+					local opts = { buffer = ev.buf }
+					vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+					vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+					vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+					vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+					vim.keymap.set('n', '<space>wl', function()
+						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+					end, opts)
+					vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+					vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+					vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+					vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+					vim.keymap.set('n', '<space>f', function()
+						vim.lsp.buf.format { async = true }
+					end, opts)
+				end,
+			})
+		end
+	}
 }
