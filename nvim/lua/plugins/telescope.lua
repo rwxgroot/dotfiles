772527f5eb38753bf5e067dac8ec66adf791e58a
Sachin <sachmalik1999@gmail.com>
2024-03-07 12:22:14 +0530

    Live grep using telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,5 +6,6 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {desc = "[F]ind [F]iles"})
+		vim.keymap.set('n', '<leader>fw', require("telescope.builtin").live_grep, {desc = "[F]ind [W]ords"})
 	end
 }
