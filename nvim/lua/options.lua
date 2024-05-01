2885ea04da32c4472ee07af1214fbec6940a98d2
Sachin <sachmalik1999@gmail.com>
2024-05-01 15:11:42 +0530

    Enable relative line numbers

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -10,10 +10,10 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.updatetime = 300
 vim.o.termguicolors = true
-vim.o.clipboard = 'unnamedplus'
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.relativenumber = true
 
 vim.keymap.set("n", "<leader>sa", "ggVG")
 vim.keymap.set("n", "//", ":noh<return>")
@@ -29,9 +29,9 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
