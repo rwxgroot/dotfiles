23ab7f9303d168d973312d81fdd151efbad45ff9
Sachin <sachmalik1999@gmail.com>
2024-07-23 16:27:55 +0530

    Use prettierd

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -68,7 +68,7 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.prettierd,
 				},
 			})
 
