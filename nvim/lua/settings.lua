fe22aa6925c0e8610de865188db660b90087d7ae
Sachin Malik <sachmalik1999@gmail.com>
2025-02-08 14:58:52 +0530

    Enable colorcolumn

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -32,5 +32,10 @@ vim.o.smartcase = true
 -- Sync with system clipboard
 vim.o.clipboard = "unnamedplus"
 
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = "80"
+--- except in Rust where the rule is 100 characters
+vim.api.nvim_create_autocmd("Filetype", { pattern = "rust", command = "set colorcolumn=100" })
+
 -- Enable inlay_hint
 vim.lsp.inlay_hint.enable(true)
